bbcf316e48583515ee5655518781a75b028cf43d
PoisonAlien <anandmt3@gmail.com>
2021-01-10 13:01:06 +0100

    update

--- src/somaticfreq.c
+++ src/somaticfreq.c
@@ -12,6 +12,7 @@ void is_bam(char *fname);
 void printhead(FILE *fn, char *bam_fn);
 void printrow(FILE *fn,  char *chrom, char *start, char *ref, char *alt, char *gene, char *vc, char *pc, char *cosid, float vaf, float counts[]);
 void printargtbl(FILE *fn, int mapq, char *bam, int tot_var, int som_vars, float vaf, int cov, float doc, int treads);
+void printfooter(FILE *fn);
 char *basename(char const *path);
 char *removeExt(char* myStr);
 
@@ -207,7 +208,6 @@ int main (int argc, char **argv){
         int32_t tot_reads = 0;
         float nt[6] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
         //char *gt = "NA";
-        float vaf = 0.0;
 
         vars_gt = vars_gt + 1;
 
@@ -307,6 +307,7 @@ int main (int argc, char **argv){
         }
 
         int pass_treads = 1; //Tumor supporting reads
+        float vaf = 0.0;
         if(strcmp(vc, "INS") == 0){
             if(nt[4] < t){
                     pass_treads = 0;
@@ -351,6 +352,7 @@ int main (int argc, char **argv){
         hts_itr_destroy(samitr);
         fprintf(tsv_fp, "\t%.3f\t%.f\t%.f\t%.f\t%.f\t%.f\t%.f\n", vaf, nt[0], nt[1], nt[2], nt[3], nt[4], nt[5]);
         //write nt counts as table row
+        fprintf(stderr, "%s\t%s\t%.3f\n", ref, alt, vaf);
         if(!isnan(vaf)){
             //fprintf(stderr, "%s\t%s\t%.3f\n", ref, alt, vaf);
             if(vaf > v && tot_reads > d && pass_treads == 1){
@@ -363,6 +365,7 @@ int main (int argc, char **argv){
     mean_doc = mean_doc/vars_gt;
     //Close all open connections and destroy destroy objects    
     printargtbl(html_fp, q, bn, vars_gt, som_vars, v, d, mean_doc, t);
+    printfooter(html_fp);
     bam_destroy1(aln);
     bam_hdr_destroy(bamHdr);
     sam_close(fp_in);
@@ -432,7 +435,7 @@ void printhead(FILE *fn, char *bam_fn){
     fprintf(fn, "<head>\n");
         fprintf(fn, "<meta charset=\"UTF-8\">\n");
         fprintf(fn, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n");
-        fprintf(fn , "<title>%s | COSMIC variants</title>\n", bam_fn);
+        fprintf(fn , "<title>%s | somatic variants</title>\n", bam_fn);
         fprintf(fn, "<link rel=\"stylesheet\" href=\"https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css\">\n");
         fprintf(fn, "<script src=\"https://code.jquery.com/jquery-3.5.1.js\"></script>\n");
         fprintf(fn, "<script src=\"https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js\"></script>\n");
@@ -443,7 +446,7 @@ void printhead(FILE *fn, char *bam_fn){
         fprintf(fn, "th, td {padding: 12px; text-align: left; border-bottom: 1px solid #ddd;}\n");
         fprintf(fn, "#header {background-color: #2c3e50; color: #fff;}\n");
         fprintf(fn, "h1 {font-weight: 600; text-align: left; color: #2c3e50; padding: 10px 0px;}    \n");
-        fprintf(fn, "h3 {text-align: left; color: #c0392b}\n");
+        fprintf(fn, "h3 {text-align: left; color: #c0392b; padding: 5px}\n");
         fprintf(fn, "tr:hover {background-color: #f5f5f5; transform: scale(1.02); box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.2), -1px -1px 8px rgba(0, 0, 0, 0.2);}\n");
         fprintf(fn, "@media only screen and (max-width: 768px) {table {width: 90%%;}}\n");
         fprintf(fn, ".details tr { line-height: 15px; }\n");
@@ -456,7 +459,16 @@ void printhead(FILE *fn, char *bam_fn){
     fprintf(fn, "</head>\n");
 
 
-    fprintf(fn, "<h2>Filtered variants </h2>\n");
+    //fprintf(fn, "<h3 >somaticfreq report </h3>");
+    fprintf(fn, "<h3> somaticfreq report</h3>");
+
+    time_t t = time(NULL);
+    struct tm cutime = *localtime(&t);
+    fprintf(fn, "<table class=\"details\">\n");
+    fprintf(fn, "<tr><td style=\"font-weight:bold\">Date</td><td>%d-%02d-%02d %02d:%02d:%02d</td></tn>", cutime.tm_year + 1900, cutime.tm_mon + 1, cutime.tm_mday, cutime.tm_hour, cutime.tm_min, cutime.tm_sec);
+    fprintf(fn, "<tr><td style=\"font-weight:bold\">Sample</td><td>%s</td></tn>", bam_fn);
+    fprintf(fn, "</table>\n");
+
     fprintf(fn, "<p style=\"margin-top:2.5em\"> \n</p>\n");
     fprintf(fn, "<p style=\"margin-top:2.5em\"> \n</p>\n");
     fprintf(fn, "<p style=\"margin-top:2.5em\"> \n</p>\n");
@@ -466,14 +478,10 @@ void printhead(FILE *fn, char *bam_fn){
     fprintf(fn, "<tbody>\n");
 }
 
-void printargtbl(FILE *fn, int mapq, char *bam, int tot_var, int som_vars, float vaf, int cov, float doc, int treads){
-
-    time_t t = time(NULL);
-    struct tm cutime = *localtime(&t);
+void printargtbl(FILE *fn, int mapq, char *bam, int tot_var, int som_vars, float vaf, int cov, float doc, int treads){    
     fprintf(fn, "</table>\n"); //end previous table before printing summary
     fprintf(fn, "<h3 >Summary </h3>");
     fprintf(fn, "<table class=\"details\">\n");
-    fprintf(fn, "<tr><td style=\"font-weight:bold\">Date</td><td>%d-%02d-%02d %02d:%02d:%02d</td></tn>", cutime.tm_year + 1900, cutime.tm_mon + 1, cutime.tm_mday, cutime.tm_hour, cutime.tm_min, cutime.tm_sec);
     fprintf(fn, "<tr><td style=\"font-weight:bold\">BAM</td><td>%s</td></tn>", bam);
     fprintf(fn, "<tr><td style=\"font-weight:bold\">Variants queried</td><td>%d</td></tn>", tot_var);
     fprintf(fn, "<tr><td style=\"font-weight:bold\">Variants with somatic evidance</td><td>%d [VAF > %.2f]</td></tn>", som_vars, vaf);
@@ -492,6 +500,13 @@ void printargtbl(FILE *fn, int mapq, char *bam, int tot_var, int som_vars, float
 void printrow(FILE *fn,  char *chrom, char *start, char *ref, char *alt, char *gene, char *vc, char *pc, char *cosid, float vaf, float counts[]){
     fprintf(fn, "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td><a target=\"_blank\" href=\"https://cancer.sanger.ac.uk/cosmic/gene/analysis?ln=%s#variants\"> %s</a></td><td>%s</td><td>%s</td><td>%s</td><td>%.3f</td><td>%.f</td><td>%.f</td><td>%.f</td><td>%.f</td><td>%.f</td><td>%.f</td></tr>\n", 
         chrom, start, ref, alt, gene, gene, vc, pc, cosid, vaf, counts[0], counts[1], counts[2], counts[3], counts[4], counts[5]);
+}
+
+void printfooter(FILE *fn){
+    fprintf(fn, "<p style=\"margin-top:2.5em\"> \n</p>\n");
+    fprintf(fn, "<div id=\"footer\">\n");
+    fprintf(fn, "<span style=\"float:left;font-family:'Courier New', Courier, monospace; padding: 5px;\" >Generated by <a href=\"https://github.com/PoisonAlien/somaticfreq\">somaticfreq</a></span>");
+    fprintf(fn, "</div>");
 }
 
 void usage(){
